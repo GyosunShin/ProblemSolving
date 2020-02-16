50c96f28dcfac317ecd332dde3c92113418196b7
gyosun <gyosunshin@gmail.com>
2020-02-16 13:42:44 +0900

    priorityqueue 사용법 주석 변경

--- priority queue 관련/priorityqueue.cpp
+++ priority queue 관련/priorityqueue.cpp
@@ -2,7 +2,7 @@
 #include <queue>
 using namespace std;
  
-// 씨발 원래 greater는 내림차순이라는데 왜 pq에 쓰이면 Min Heap을 만들어주는거지.... 씨발... 
+// 원래 greater는 내림차순이라는데 왜 pq에 쓰이면 Min Heap을 만들어주는거지
 // 그니까 greater 쓰면 오름차순(작은 것 부터) 
 priority_queue<int, vector<int>, greater<int>> pq;  
 
